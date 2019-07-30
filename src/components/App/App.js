1ca46acc38616f4dccd17d35defbfc11008c9d82
Adel <htwr86@gmail.com>
2019-07-30 20:37:14 +0300

    add Event component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from '../Header';
+import Event from '../Event';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Hockey Team Manager App</h2>
-      
+    <div className='container'>
+      <Header />
+      <Event />
     </div>
+    
   );
 }
 
---
+++ src/components/Event/Event.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Event extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Event</h2>
+      </div>
+    )
+  }
+}
+
+export default Event;
