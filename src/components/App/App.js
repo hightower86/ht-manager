4fa5b5b3fcdfa6e3571f73d06655de3dfee46b02
Adel <htwr86@gmail.com>
2019-07-30 21:09:58 +0300

    player list in Event Component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,14 +3,41 @@ import './App.css';
 import Header from '../Header';
 import Event from '../Event';
 
-function App() {
-  return (
-    <div className='container'>
+class App extends React.Component {
+
+  state = {
+    players: [
+      {
+        id: 1,
+        name: "Белый Александр"
+      },
+      {
+        id: 2,
+        name: "Воднев Максим"
+      },
+      {
+        id: 3,
+        name:"Великий Сергей"
+      },
+      {
+        id: 4,
+        name:"Гусев Алексей"
+      }
+    ]
+  };
+
+  render() {
+
+    return (
+      <div>
+        <div className='container'>
       <Header />
-      <Event />
+      <Event players={this.state.players}/>
     </div>
-    
-  );
+      </div>
+    );
+  }
+  
 }
 
 export default App;
--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react';
 
 export class Event extends Component {
   render() {
+    const players = this.props.players;
     return (
       <div>
-        <h2>Event</h2>
+      <h4>Player list</h4>
+      <ul>
+        {
+          players.map(({ id, name }) => {
+            return <li key={id}>  {name}</li>
+          })
+        }
+      </ul>
+        
       </div>
     )
   }
