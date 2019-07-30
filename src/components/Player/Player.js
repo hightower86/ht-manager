5bf02706ef19ac89159f6eddbd48381e3286fd07
Adel <htwr86@gmail.com>
2019-07-30 23:08:59 +0300

    player component

---
+++ src/components/Player/Player.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import './Player.css';
+
+export class Player extends Component {
+
+  state = {
+    id: 'id',
+    name: 'name',
+    inGame: 0
+  }
+  render() {
+    return (
+      <div className='player'>
+        
+      </div>
+    )
+  }
+}
+
+export default Player
--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import Player from '../Player';
+import './Team.css';
 
 export class Team extends Component {
+
   render() {
+
     const players = this.props.players;
-    console.log(players);
+
     return (
       <div>
-        <ul className="list-group">
+        <ul className="list-group players">
         {
           players.map(({ id, name }) => {
             return <li className='list-group-item d-flex justify-content-between align-items-center' key={id}>  {name}</li>
